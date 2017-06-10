3e273e1539aba2e15cc25ee8742afffab9466caa
Aurora Banuelos <aurorabanuelos@zipcoder5.local>
2017-06-10 17:28:17 -0400

    update destination map

--- src/pages/destination/destination.ts
+++ src/pages/destination/destination.ts
@@ -30,7 +30,6 @@ export class DestinationPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad DestinationPage');
     this.loadMap();
-    this.startNavigation();
     // this._destinationService.getDestinations().subscribe(data => {
 		// 		console.log(data);
 			// }
@@ -51,33 +50,22 @@ export class DestinationPage {
 
             this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
 
-            let marker = new google.maps.Marker({})
 
-        });
-    }
+            let infowindow = new google.maps.InfoWindow({
+                content: "Destination"
+            });
 
-    startNavigation() {
+            let destinationLoc = {lat: 39.757203, lng: -75.563795};
 
-        //change to ion native loction import because it asks the device to use its location
-        navigator.geolocation.getCurrentPosition(position => {
+            let marker = new google.maps.Marker({
+                position: destinationLoc,
+                map: this.map,
+            });
 
-        const directionsService = new google.maps.DirectionsService;
-        const directionsDisplay = new google.maps.DirectionsRenderer;
-
-        directionsDisplay.setMap(this.map);
-        directionsDisplay.setPanel(this.directionPanel.nativeElement);
-
-        directionsService.route({
-            origin: {lat: position.coords.latitude, lng: position.coords.longitude},
-            destination: {lat: 39.788278, lng: -75.545414},
-            travelMode: google.maps.TravelMode['DRIVING']
-        }, (res, status) => {
-            if (status === google.maps.DirectionsStatus.OK){
-                directionsDisplay.setDirections(res);
-            }else {
-                console.log("Error Loading Directions");
-            }
-        });
+            marker.addListener ('click', function() {
+                infowindow.open(this.map, marker);
+            });
+            
 
         });
     }
@@ -94,8 +82,5 @@ export class DestinationPage {
   goToThankYou() {
     this.navCtrl.push(VolThankYouPage);
   }
-
-  //  lat: number = 39.7391;
-  //  lng: number = -75.5398;
 
 }
