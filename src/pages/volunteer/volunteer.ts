2d8608c5640c6442317edb5ec122a3fc8c3a70ec
Byung-Chun Yoo <byung-chunyoo@zipcoder7.local>
2017-06-10 16:10:28 -0400

    pickupLoc added to Volunteer.ts

--- src/pages/volunteer/volunteer.ts
+++ src/pages/volunteer/volunteer.ts
@@ -29,7 +29,7 @@ export class VolunteerPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad VolunteerPage');
     this.loadMap();
-    this.startNavigation();
+    // this.startNavigation();
   }
 
   loadMap() {
@@ -39,43 +39,58 @@ export class VolunteerPage {
 
             const mapOptions = {
                 center: latlng,
-                zoom: 15,
+                zoom: 13,
                 mapTypeId: google.maps.MapTypeId.ROADMAP,
                 streetViewControl: false
             };
 
             this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
 
-            let marker = new google.maps.Marker({})
+            let contentString = "Acme at Trolley"
 
+            let infowindow = new google.maps.InfoWindow({
+                content: contentString
+            });
+
+            let pickupLoc = {lat: 39.757203, lng: -75.563795};
+
+            let marker = new google.maps.Marker({
+                position: pickupLoc,
+                map: this.map,
+                title: "Acme at Trolley"
+            });
+
+            marker.addListener ('click', function() {
+                infowindow.open(this.map, marker);
+            });
         });
     }
 
-    startNavigation() {
+    // startNavigation() {
 
-        //change to ion native loction import because it asks the device to use its location
-        navigator.geolocation.getCurrentPosition(position => {
+    //     //change to ion native loction import because it asks the device to use its location
+    //     navigator.geolocation.getCurrentPosition(position => {
 
-        const directionsService = new google.maps.DirectionsService;
-        const directionsDisplay = new google.maps.DirectionsRenderer;
+    //     const directionsService = new google.maps.DirectionsService;
+    //     const directionsDisplay = new google.maps.DirectionsRenderer;
 
-        directionsDisplay.setMap(this.map);
-        directionsDisplay.setPanel(this.directionPanel.nativeElement);
+    //     directionsDisplay.setMap(this.map);
+    //     directionsDisplay.setPanel(this.directionPanel.nativeElement);
 
-        directionsService.route({
-            origin: {lat: position.coords.latitude, lng: position.coords.longitude},
-            destination: {lat: 39.788278, lng: -75.545414},
-            travelMode: google.maps.TravelMode['DRIVING']
-        }, (res, status) => {
-            if (status === google.maps.DirectionsStatus.OK){
-                directionsDisplay.setDirections(res);
-            }else {
-                console.log("Error Loading Directions");
-            }
-        });
+    //     directionsService.route({
+    //         origin: {lat: position.coords.latitude, lng: position.coords.longitude},
+    //         destination: {lat: 39.788278, lng: -75.545414},
+    //         travelMode: google.maps.TravelMode['DRIVING']
+    //     }, (res, status) => {
+    //         if (status === google.maps.DirectionsStatus.OK){
+    //             directionsDisplay.setDirections(res);
+    //         }else {
+    //             console.log("Error Loading Directions");
+    //         }
+    //     });
 
-        });
-    }
+    //     });
+    // }
 
   goToDestination() {
     this.navCtrl.push(DestinationPage);
