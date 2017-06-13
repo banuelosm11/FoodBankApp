465f99d31a41b5277ab2dafad1709e8b9a60b689
Aurora Banuelos <aurorabanuelos@zipcoder5.local>
2017-06-13 12:01:04 -0400

    update

--- src/pages/destination/destination.ts
+++ src/pages/destination/destination.ts
@@ -46,7 +46,7 @@ export class DestinationPage {
 
 loadDirections(pickUpDropOff:any) {
 
-   // navigator.geolocation.getCurrentPosition(position => {
+   navigator.geolocation.getCurrentPosition(position => {
 
             const directionsService = new google.maps.DirectionsService;
             const directionsDisplay = new google.maps.DirectionsRenderer({
@@ -55,7 +55,7 @@ loadDirections(pickUpDropOff:any) {
 
     //https://lh3.googleusercontent.com/5OM8W6oF0NdKd_8aEKlpSybDejudy-AFsxT6E3p_Acb9iLNCrdQXwhXwJhsNcVAJNhs=w300
             let list: any[] = [
-                {latlng: new google.maps.LatLng(39.7708, -75.5597),
+                {latlng: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
                     image: {
                     url: 'http://www.freeiconspng.com/uploads/name-people-person-user-icon--icon-search-engine-1.png',
                     scaledSize: new google.maps.Size(30, 30),
@@ -135,7 +135,7 @@ loadDirections(pickUpDropOff:any) {
                 }
             });
 
-            // });
+            });
  }
 
   goToThankYou() {
